d33a2fbda48ddda959b4a9b4ec671a03eca14578
khal_huntsman <hunter.jude.steele@gmail.com>
2025-12-18 02:36:26 -0600

    made changes to lab after getting pandas installed and being able to run the file and testing to correct mistakes

--- main.py
+++ main.py
@@ -6,60 +6,62 @@ conn = sqlite3.connect('data.sqlite')
 
 # STEP 2
 df_first_five = pd.read_sql("""
-SELECT employee_number, lastName
-    FROM employees;
-""", conn).head()
+SELECT employeeNumber, lastName
+FROM employees;
+""", conn)
 
 # STEP 3
 df_five_reverse = pd.read_sql("""
-SELECT lastName, employee_number
-    FROM employees;
-""", conn).head()
+SELECT lastName, employeeNumber
+FROM employees;
+""", conn)
 
 # STEP 4
 df_alias = pd.read_sql("""
-SELECT employee_number AS ID
-    FROM employees;
-""", conn).head()
+SELECT employeeNumber AS ID
+FROM employees;
+""", conn)
 
 # STEP 5
 df_executive = pd.read_sql("""
 SELECT firstName, lastName, jobTitle,
-        CASE
-            WHEN jobTitle = "President" 
-                OR jobTitle = "VP Sales" 
-                OR jobTitle = "VP Marketing" 
-            THEN "Executive"
-            ELSE "Not Executive"
-        END AS role
-    FROM employees;
-""", conn).head()
+       CASE
+         WHEN jobTitle = "President"
+           OR jobTitle = "VP Sales"
+           OR jobTitle = "VP Marketing"
+         THEN "Executive"
+         ELSE "Not Executive"
+       END AS role
+FROM employees;
+""", conn)
 
 # STEP 6
 df_name_length = pd.read_sql("""
 SELECT length(lastName) AS name_length
-    FROM employees;
-""", conn).head()
+FROM employees;
+""", conn)
 
 # STEP 7
 df_short_title = pd.read_sql("""
-SELECT substr(jobTitle, 1,2) AS short_title
-    FROM employees;
-""", conn).head()
+SELECT substr(jobTitle, 1, 2) AS short_title
+FROM employees;
+""", conn)
 
 # STEP 8
 sum_total_price = pd.read_sql("""
-SELECT SUM(ROUND(priceEach * quantityOrdered, 0)) AS total_amount
+SELECT ROUND(priceEach * quantityOrdered, 0) AS total_price
 FROM orderDetails;
-""", conn).head()
+""", conn).sum()
 
 # STEP 9
 df_day_month_year = pd.read_sql("""
-SELECT orderDate,
-        strftime("%d", orderDate) AS day,
-        strftime("%m", orderDate) AS month,
-        strftime("%Y", orderDate) AS year
-    FROM orderDetails;
-""", conn).head()
+SELECT o.orderDate,
+       strftime('%d', o.orderDate) AS day,
+       strftime('%m', o.orderDate) AS month,
+       strftime('%Y', o.orderDate) AS year
+FROM orders o
+JOIN orderDetails od
+  ON o.orderNumber = od.orderNumber;
+""", conn)
 
 conn.close()
